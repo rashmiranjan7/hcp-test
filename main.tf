c7a15331395829a6365d3e03b07fcaec7e4d8b76
Rashmi ranjan panigrahy <rashmiranjanpanigrahy07@gmail.com>
2025-09-10 10:28:55 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,13 @@
 provider "aws" {
-  region = "ap-south-1" 
+  region = "ap-south-2" 
 }
 
 resource "aws_instance" "example" {
-  count         = 3
-  ami           = "ami-0861f4e788f5069dd"
+  count         = 5
+  ami           = "ami-0bd4cda58efa33d23"
   instance_type = "t2.micro"
 
   tags = {
-    Name = "Terraform-EC2"
+    Name = "Main-server-1"
   }
 }
